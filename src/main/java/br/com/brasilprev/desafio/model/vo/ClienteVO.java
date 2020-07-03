2849bb108a8a22500d708c9db41061614b11b8d3
Bruno Rocha <bruno.ahcor@gmail.com>
2020-07-03 03:49:26 -0300

    Added validators

--- src/main/java/br/com/brasilprev/desafio/model/vo/ClienteVO.java
+++ src/main/java/br/com/brasilprev/desafio/model/vo/ClienteVO.java
@@ -2,6 +2,12 @@ package br.com.brasilprev.desafio.model.vo;
 
 import java.time.LocalDateTime;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -16,9 +22,17 @@ import lombok.ToString;
 public class ClienteVO {
 
     private Long id;
+    @NotBlank(message = "{nome.not.blank}")
     private String nome;
+    @Pattern(
+        regexp = "^\\d{3}\\.\\d{3}\\.\\d{3}\\-\\d{2}$",
+        message = "{cpf.not.valid}"
+    )
     private String cpf;
+    @NotBlank(message = "{email.not.blank}")
+    @Email(message = "{email.not.valid}")
     private String email;
+    @JsonIgnore
     private LocalDateTime dataCadastro;
     
 }
--- src/main/java/br/com/brasilprev/desafio/model/vo/ProdutoVO.java
+++ src/main/java/br/com/brasilprev/desafio/model/vo/ProdutoVO.java
@@ -3,6 +3,10 @@ package br.com.brasilprev.desafio.model.vo;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -17,8 +21,15 @@ import lombok.ToString;
 public class ProdutoVO {
 
     private Long id;
+    @NotBlank
     private String nome;
     private BigDecimal valorUnidade;
+    @JsonIgnore
     private LocalDateTime dataCadastro;
+
+    public ProdutoVO(String nome, BigDecimal valorUnidade) {
+        this.nome = nome;
+        this.valorUnidade = valorUnidade;
+    }
 
 }

31539e6ca48b73bd40eba1206d78ac1728d3bcfd
Bruno Rocha <bruno.ahcor@gmail.com>
2020-07-04 18:04:15 -0300

    Added validations

---
+++ src/main/java/br/com/brasilprev/desafio/util/Regex.java
@@ -0,0 +1,8 @@
+package br.com.brasilprev.desafio.util;
+
+public class Regex {
+
+    public static final String NOT_NUMBER = "[^\\d]*";
+    public static final String EMAIL = "^[A-Za-z0-9+_.-]+@(.+)$";
+    
+}

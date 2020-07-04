cc356ee60a1aef096d1fe18b8c69b70a13386efe
Bruno Rocha <bruno.ahcor@gmail.com>
2020-07-04 18:09:36 -0300

    Added business rule

--- src/main/java/br/com/brasilprev/desafio/service/ClienteService.java
+++ src/main/java/br/com/brasilprev/desafio/service/ClienteService.java
@@ -3,18 +3,19 @@ package br.com.brasilprev.desafio.service;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import br.com.brasilprev.desafio.exception.BusinessException;
 import br.com.brasilprev.desafio.model.vo.ClienteVO;
 
 public interface ClienteService {
 
-    ClienteVO salvar(ClienteVO vo);
-
     Page<ClienteVO> listar(Pageable pageable);
 
-    ClienteVO buscarPorId(Long id);
+    ClienteVO salvar(ClienteVO vo) throws Exception;
 
-    ClienteVO buscarPorCpf(String cpf);
+    ClienteVO buscarPorId(Long id) throws Exception ;
 
-    void deletar(Long id);
+    ClienteVO buscarPorCpf(String cpf) throws BusinessException;
+
+    void deletar(Long id) throws Exception;
     
 }
--- src/main/java/br/com/brasilprev/desafio/service/ClienteServiceImpl.java
+++ src/main/java/br/com/brasilprev/desafio/service/ClienteServiceImpl.java
@@ -10,12 +10,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import br.com.brasilprev.desafio.converter.ClienteConverter;
+import br.com.brasilprev.desafio.exception.BusinessException;
 import br.com.brasilprev.desafio.model.entity.Cliente;
 import br.com.brasilprev.desafio.model.vo.ClienteVO;
 import br.com.brasilprev.desafio.repository.ClienteRepository;
+import br.com.brasilprev.desafio.util.Validador;
 
 @Service
 public class ClienteServiceImpl implements ClienteService {
@@ -27,7 +30,16 @@ public class ClienteServiceImpl implements ClienteService {
     private ClienteConverter converter;
 
     @Override
-    public ClienteVO salvar(ClienteVO vo){
+    public ClienteVO salvar(ClienteVO vo) throws Exception {
+        if( !Optional.ofNullable( vo.getNome() ).isPresent() ){
+            throw new BusinessException("NOME nao informado.",HttpStatus.BAD_REQUEST);
+        }
+        if( !Validador.isCPF(vo.getCpf()) ){
+            throw new BusinessException("CPF nao informado ou invalido.",HttpStatus.BAD_REQUEST);
+        }
+        if( Validador.isEmail(vo.getEmail()) ){
+            throw new BusinessException("EMAIL nao informado ou invalido.",HttpStatus.BAD_REQUEST);
+        }
         vo.setDataCadastro( Optional.ofNullable(vo.getDataCadastro()).orElse(LocalDateTime.now()) );
         Cliente cliente = clienteRepository.save( converter.toEntity(vo) );
         return converter.toVO(cliente);
@@ -41,19 +53,30 @@ public class ClienteServiceImpl implements ClienteService {
     }
 
     @Override
-    public ClienteVO buscarPorId(Long id){
+    public ClienteVO buscarPorId(Long id) throws Exception {
         Optional<Cliente> cliente = clienteRepository.findById(id);
-        return cliente.isPresent() ? converter.toVO(cliente.get()) : null; 
+        if(cliente.isPresent()){
+            return converter.toVO(cliente.get());
+        } else {
+            throw new BusinessException("Cliente nao encontrado com ID informado.",HttpStatus.NO_CONTENT);
+        }
     }
 
     @Override
-    public ClienteVO buscarPorCpf(String cpf){
+    public ClienteVO buscarPorCpf(String cpf) throws BusinessException {
+        if( !Optional.ofNullable( cpf ).isPresent() || !Validador.isCPF(cpf) ){
+            throw new BusinessException("CPF nao informado.",HttpStatus.BAD_REQUEST);
+        }
         Optional<Cliente> cliente = clienteRepository.findByCpf(cpf);
+        if(!cliente.isPresent()){
+            throw new BusinessException("Cliente nao encontrado com CPF informado.",HttpStatus.NO_CONTENT);
+        }
         return cliente.isPresent() ? converter.toVO(cliente.get()) : null;
     }
 
     @Override
-    public void deletar(Long id){
+    public void deletar(Long id) throws Exception {
+        buscarPorId(id);
         clienteRepository.deleteById(id);
     }
     

a8f4cedbcd022713c8888beae0ac38bdbe706709
Bruno Rocha <bruno.ahcor@gmail.com>
2020-07-04 18:05:46 -0300

    Changed custom exceptions

--- src/main/java/br/com/brasilprev/desafio/exception/BusinessException.java
+++ src/main/java/br/com/brasilprev/desafio/exception/BusinessException.java
@@ -1,5 +1,22 @@
 package br.com.brasilprev.desafio.exception;
 
-public class BusinessException extends RuntimeException  {
+import org.springframework.http.HttpStatus;
+
+public class BusinessException extends RuntimeException {
+
+    private HttpStatus status;
+
+    public HttpStatus getStatus() {
+        return this.status;
+    }
+
+    public BusinessException(String errorMessage) {
+        super(errorMessage);
+    }
+
+    public BusinessException(String errorMessage, HttpStatus status) {
+        super(errorMessage);
+        this.status = status;
+    }
     
 }
--- src/main/java/br/com/brasilprev/desafio/exception/IntegrationException.java
+++ src/main/java/br/com/brasilprev/desafio/exception/IntegrationException.java
@@ -1,5 +1,22 @@
 package br.com.brasilprev.desafio.exception;
 
+import org.springframework.http.HttpStatus;
+
 public class IntegrationException extends RuntimeException {
+
+    private HttpStatus status;
+
+    public HttpStatus getStatus() {
+        return this.status;
+    }
+
+    public IntegrationException(String errorMessage) {
+        super(errorMessage);
+    }
+
+    public IntegrationException(String errorMessage, HttpStatus status) {
+        super(errorMessage);
+        this.status = status;
+    }
     
 }
